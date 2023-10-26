19f6cf2b1ca47ea6d3235b5bcb117f4ab6c0bb7d
sheftechdad <shefalichaurasia30945@gmail.com>
2023-10-26 13:04:50 +0530

    add new foood file

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from turtle import Turtle,Screen
 import time
+from scoreboard import Scoreboard
+from food import Food
 from snake import Snake
+
+
 screen=Screen()
 screen .setup(width=600,height=600)
 screen.bgcolor("black")
@@ -10,6 +14,8 @@ segment=[]
 screen.tracer(0)
 
 snake = Snake()
+food=Food()
+scoreboard=Scoreboard()
 
 
 screen.listen()
@@ -23,8 +29,22 @@ screen.onkey(snake.right,"Right")
 is_game_start=True
 while is_game_start:
     screen.update()
-    time.sleep(0.1)
+    time.sleep(0.12)
     snake.move()
 
 
+
+    if snake.head.distance(food)<13:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_scoreboard()
+
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() >280 or snake.head.ycor() <-280:
+        is_game_start=False
+        scoreboard.game_over()
+    #detection collision with  tail
+    for segment in snake.segment[1:]:
+        if snake.head.distance(segment) <10:
+            is_game_start=False
+            scoreboard.game_over()
 screen.exitonclick()
